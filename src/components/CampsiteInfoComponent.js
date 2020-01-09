ab709a5f6394e76e4d14dfac07089c99b5019af4
Angela Canning <amc@angelacanning.com>
2020-01-09 13:29:36 -0600

    Breadcrumbs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle, CardText } from 'reactstrap';
+import { Card, CardImg, CardBody, CardText, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 function RenderCampsite({campsite}) {
     return (
         <div className="col-md-5 m-1">
-                <Card onClick={() => this.onCampsiteSelect(campsite)}>
-                    <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                    {/* <CardImgOverlay>
-                    </CardImgOverlay> */}
-                    <CardTitle>{campsite.name}</CardTitle>
-                    <CardText>{campsite.description}</CardText>
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
                 </Card>
     </div>
     );
@@ -40,6 +40,16 @@ function CampsiteInfo(props) {
     if (props.campsite) {
         return (
             <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                    <h2>{props.campsite.name}</h2>
+                    <hr />
+                </div>
+            </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments comments={props.comments} />             
